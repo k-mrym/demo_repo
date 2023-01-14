04973b2d4b001c402630710fefb5b6832b08112c
bisdev-moriyama\moriyama <moriyama.dev@gmail.com>
2023-01-15 04:40:48 +0900

    add & fix

--- sh/postgresql/dump_insert.sh
+++ sh/postgresql/dump_insert.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 declare -r COTAINER_NAME=XXXX_postgres-1
+declare -r DATABASE=XXXXdb
+declare -r USER=XXXXuser
 declare -r TABLE_NAME=dtb_XXXX
 
-# pg_dump --username=dbuser --table ${TABLE_NAME} --data-only --column-inserts eccubedb > ${TABLE_NAME}.sql
+pg_dump --username=${USER} --table ${TABLE_NAME} --data-only --column-inserts ${DATABASE} > ${TABLE_NAME}.sql
 
-docker exec ${COTAINER_NAME} pg_dump --username=dbuser --table ${TABLE_NAME} --data-only --column-inserts eccubedb > ${TABLE_NAME}.sql
+# docker exec ${COTAINER_NAME} pg_dump --username=${USER} --table ${TABLE_NAME} --data-only --column-inserts ${DATABASE} > ${TABLE_NAME}.sql
 
 cat ${TABLE_NAME}.sql
 rm ${TABLE_NAME}.sql
